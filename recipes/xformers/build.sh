4368d9ecc0dc666185dba7f44fef677e05864c84
rxm7706 <95496360+rxm7706@users.noreply.github.com>
2025-04-29 04:36:48 -0500

    xformers

---
+++ recipes/xformers/build.sh
@@ -0,0 +1,23 @@
+set -ex
+
+# Target the same CUDA archs as conda-forge pytorch package
+# https://github.com/conda-forge/pytorch-cpu-feedstock/blob/main/recipe/build_pytorch.sh
+# Number of CUDA archs reduced to fit CI resources
+if [[ ${cuda_compiler_version} != "None" ]]; then
+    if [[ ${cuda_compiler_version} == 11.8 ]]; then
+        export TORCH_CUDA_ARCH_LIST="5.3;6.0;6.1;7.0;7.5;8.0;8.6;8.9+PTX"
+    elif [[ ${cuda_compiler_version} == 12.6 ]]; then
+        export TORCH_CUDA_ARCH_LIST="5.3;6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0+PTX"
+    else
+        echo "Unsupported CUDA compiler version. Edit build.sh to add target CUDA archs."
+        exit 1
+    fi
+    export TORCH_NVCC_FLAGS="-Xfatbin -compress-all"
+fi
+
+# avoid "error: 'value' is unavailable: introduced in macOS 10.13"
+export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+
+export BUILD_VERSION=${package_version}
+
+$PYTHON -m pip install . -vv --no-deps --no-build-isolation
