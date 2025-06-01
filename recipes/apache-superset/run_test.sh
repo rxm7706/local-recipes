42b63cfd662beb53f7cd8430fecd015f012a8314
rxm7706 <95496360+rxm7706@users.noreply.github.com>
2025-06-01 09:32:22 -0500

    apache-superset Conda Build works for python # [py<39 or py>311]

--- recipes/apache-superset/run_test.sh
+++ recipes/apache-superset/run_test.sh
@@ -2,9 +2,13 @@
 
 set -ex
 
+echo "START_MESSAGE: run_test.sh"
+echo "START_MESSAGE: export flask settings"
 export FLASK_APP='superset'
 export SUPERSET_SECRET_KEY=$(openssl rand -base64 42)
+echo "START_MESSAGE: superset db upgrade"
 superset db upgrade
+echo "START_MESSAGE: flask fab create-admin"
 flask fab create-admin \
   --username admin \
   --firstname admin \
@@ -12,4 +16,6 @@ flask fab create-admin \
   --email admin@fab.org \
   --password admin
 # superset load_examples
+echo "START_MESSAGE: superset init"
 superset init
+echo "END_MESSAGE: run_test.sh"
